d2d42d4f409eb344a316cc0912a4d2a08388a25e
Zach Quita <zachquita@gmail.com>
2024-12-11 21:44:00 +0800

    accumulate products

--- day3/day3Part1.cpp
+++ day3/day3Part1.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <numeric>
 
 typedef std::vector<std::string> StringVector;
 
@@ -20,15 +21,17 @@ std::string ReadDataFromFile(std::string& file_name) {
     return buffer.str();
 }
 
-void SearchForInstructions(
+void SearchInputForInstructions(
     std::string& content, 
     std::smatch& match, 
     std::regex& pattern, 
-    StringVector& instructions
+    std::vector<int>& products
 ) {
     while (std::regex_search (content, match, pattern)) {
-        for (auto m : match) std::cout << m << " ";
-        std::cout << '\n';
+        // for (auto m : match) std::cout << m << " ";
+        // Calculate mul(X,Y)
+        products.push_back((std::stoi(match[1]) * std::stoi(match[2]))); 
+        // std::cout << '\n';
         content = match.suffix().str();
     }
 
@@ -39,9 +42,10 @@ int main() {
     std::string content = ReadDataFromFile(file_name);
     std::smatch match;
     std::regex pattern("mul\\((\\d{1,3}),(\\d{1,3})\\)");
-    StringVector instructions; // Will sum these up later 
+    std::vector<int> products; // Will sum these up later 
 
-    SearchForInstructions(content, match, pattern, instructions);
+    SearchInputForInstructions(content, match, pattern, products);
+    std::cout << std::accumulate(products.begin(), products.end(), 0) << "\n";
 
     return 0;
 }
