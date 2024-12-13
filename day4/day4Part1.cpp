0c435e3eefb7da15e338785b784cae01dbae89f6
Zach Quita <zachquita@gmail.com>
2024-12-13 10:30:36 +0800

    iterate through StringVector

--- day4/day4Part1.cpp
+++ day4/day4Part1.cpp
@@ -20,12 +20,41 @@ void ReadInputToVector(std::string& file_name, StringVector& words) {
     }
 }
 
+int CountXmas(StringVector& words) {
+    int count = 0;
+    for (int i = 0; i < words.size() - 4; ++i) {
+        std::cout << "line: " << i << "\n";
+        for (int j = 0; j < words[i].size() - 4; ++j) {
+            StringVector sub_words;
+            for (int k = i; k < i + 4; ++k) {
+                sub_words.emplace_back(words[k], j, 4);
+            }
+            /* sub_words looks smth like dis:
+            {   "MSAM",
+                "AMXS",
+                "MSAM",
+                "XMAS" } */
+            for (const auto& s : sub_words) {
+                std::cout << s << "\n";
+            }
+            sub_words.clear();
+            std:: cout << "\n";
+        }
+    }
+
+    return count;
+}
+
+void IterateStringVector(StringVector& vec) {
+
+}
+
 int main() {
     std::string file_name = "day4TestInput.txt";
     StringVector words;
 
     ReadInputToVector(file_name, words);
-    
+    int count = CountXmas(words);
 
     return 0;
 }
